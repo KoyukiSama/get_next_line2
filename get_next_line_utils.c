1a39f55430bb5c6769e28e849195fd032c42ff0e
kaclaes <kaclaes@student.42belgium.be>
2026-05-13 18:39:35 +0200

    add (get_next_line_utils.c), func: read_into_buff() ["reads into buffer and tracks errors of -1 return from read"], ft_strlen(), append_buff() ["appends the new buffer to the cache, errors occur and are marked by NULL return]

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,17 +6,41 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 16:23:59 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/05/13 17:50:44 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/05/13 18:37:30 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
+
+static size_t	ft_strlen(char *str);
 
 char	*append_buff(char *cache, char *buffer)
 {
+	char	*new_cache;
+	size_t	i;
 
+	new_cache = malloc(ft_strlen(cache) + ft_strlen(buffer) + 1);
+	if (!new_cache)
+		return (NULL);
+	i = 0;
+	while (*cache)
+		new_cache[i++] = *cache++;
+	while (*buffer)
+		new_cache[i++] = *buffer++;
+	return (free(cache), new_cache);
+}
+
+static size_t	ft_strlen(char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
 }
 
 int	nl(char *str)
