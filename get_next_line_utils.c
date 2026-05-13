efa354936ad8455ae51634f85b5648e676805d78
kaclaes <kaclaes@student.42belgium.be>
2026-05-13 17:52:01 +0200

    add (get_next_line_utils.c), func: nl() ["add new line search function"]

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,12 +6,37 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 16:23:59 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/05/12 22:16:27 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/05/13 17:50:44 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*append_bytes(char *cache, char *buffer);
+#include "get_next_line.h"
+#include <unistd.h>
+#include <sys/types.h>
 
-int		nl_found(char *str);
+char	*append_buff(char *cache, char *buffer)
+{
 
-void	cache_clean(char **cache);
+}
+
+int	nl(char *str)
+{
+	if (!str)
+		return (0);
+	while (*str && *str != '\n')
+		str++;
+	if (*str == '\n')
+		return (1);
+	return (0);
+}
+
+void	*read_into_buff(char *buff, int fd)
+{
+	ssize_t	bytes_read;
+
+	bytes_read = read(fd, buff, BUFF_SIZE);
+	if (bytes_read == -1)
+		return (NULL);
+	buff[bytes_read] = '\0';
+	return (buff);
+}
