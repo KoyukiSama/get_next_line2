a04caaa9dc9e9b3f2bde6eb1c45a8f64444a1eae
kaclaes <kaclaes@student.42belgium.be>
2026-05-14 23:11:55 +0200

    add (get_next_line.c), funcs: cache_get_line, cache_trim

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kaclaes <kaclaes@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 16:23:55 by kaclaes           #+#    #+#             */
-/*   Updated: 2026/05/14 17:56:20 by kaclaes          ###   ########.fr       */
+/*   Updated: 2026/05/14 23:09:39 by kaclaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char		*read_into_buff(char *buff, int fd);
 char		*append_buff(char **cache, char *buffer);
 int			nl(char *str);
 
-static char	*cache_update(int fd, char *cache);
-// static void	cache_get_line(char *line, char *cache);
-// static void	cache_trim(char *cache);
+static char	*cache_update(int fd, char **cache);
+static char	*cache_get_line(char *line, char *cache);
+static char	*cache_trim(char **cache);
 
 char	*get_next_line(int fd)
 {
@@ -30,40 +30,100 @@ char	*get_next_line(int fd)
 
 	if (BUFF_SIZE <= 0 || fd < 0 || fd >= OPEN_MAX)
 		return (NULL);
-	if (!update_cache(fd, cache[fd]))
+	if (!cache_update(fd, &cache[fd]))
 		return (NULL);
-	// cache_get_line(line, cache[fd]);
-	// cache_trim(cache[fd]);
-	return (line);
+	cache_get_line(line, cache[fd]);
+	cache_trim(cache[fd]);
+	return (cache[fd]);
 }
 
+//#include <stdio.h>
+// #include <fcntl.h>
+// int main(void)
+// {
+// 	int fd = open("test/test1.txt", O_RDONLY);
+
+// 	char *line = get_next_line(fd);
+// 	printf("%s\n", line);
+// 	line = get_next_line(fd);
+// 	printf("%s\n", line);
+// 	line = get_next_line(fd);
+// 	printf("%s\n", line);
+// 	line = get_next_line(fd);
+// 	printf("%s\n", line);
+// }
+
 // returns NULL if error occurs
-static char	*cache_update(int fd, char *cache)
+static char	*cache_update(int fd, char **cache)
 {
 	char	*buff;
 
-	if (!cache)
+	if (!*cache)
 	{
-		cache = malloc(1);
-		if (!cache)
+		*cache = malloc(1);
+		if (!*cache)
 			return (NULL);
-		cache[0] = '\0';
+		*cache[0] = '\0';
 	}
 	buff = malloc((BUFF_SIZE + 1));
 	if (!buff)
-		return (free(cache), NULL);
-	while (!nl(cache))
+		return (free(*cache), NULL);
+	while (!nl(*cache))
 	{
 		if (!read_into_buff(buff, fd))
-			return (free(buff), free(cache), NULL);
+			return (free(buff), free(*cache), NULL);
 		if (buff[0] == '\0')
-			return (free(buff), cache);
-		if (!append_buff(&cache, buff))
-			return (free(buff), free(cache), NULL);
+			return (free(buff), *cache);
+		if (!append_buff(cache, buff))
+			return (free(buff), free(*cache), NULL);
 	}
-	return (free(buff), cache);
+	return (free(buff), *cache);
 }
 
-// static void	cache_get_line(char *line, char *cache)
+static char	*cache_get_line(char *line, char *cache)
+{
+	size_t	line_size;
+	size_t	i;
 
-// static void	cache_trim(char *cache)
+	line_size = 0;
+	while (cache[line_size] != '\n' && cache[line_size])
+		line_size++;
+	line = malloc(line_size);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (cache[i] != '\n' || cache[i])
+	{
+		line[i] = cache[i];
+		i++;
+	}
+	if (cache[i] == '\n')
+		line[i++ ] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*cache_trim(char **cache)
+{
+	char	*old_cache;
+	char	*new_cache;
+	size_t	i;
+
+	old_cache = *cache;
+	while (*old_cache != '\n' && *old_cache)
+		old_cache++;
+	if (*old_cache == '\0' || *(old_cache + 1) == '\0')
+		return (free(*cache), *cache = NULL, NULL);
+	old_cache++;
+	i = 0;
+	while (old_cache[i])
+		i++;
+	new_cache = malloc(i + 1);
+	if (!new_cache)
+		return (free(*cache), *cache = NULL, NULL);
+	i = 0;
+	while (*old_cache)
+		new_cache[i++] = *old_cache++;
+	new_cache[i] = '\0';
+	return (free(*cache), *cache = new_cache, *cache);
+}
